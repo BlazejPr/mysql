f335a16888ff1f8525eb000ec3f0dc2e842bccd5
BK <89515752+BlazejPr@users.noreply.github.com>
2025-05-27 08:02:24 +0200

    Update binlogi.sh

--- binlogi.sh
+++ binlogi.sh
@@ -3,3 +3,11 @@ mysql> show binlog events in 'bin-log.000001';
 
 mysqlbinlog -v --base64-output=DECODE-ROWS --start-position=196 /var/log/mysql/bin-log.000001
 
+
+
+udzyskiwanie danych: narzędzie:MyUndelete
+
+https://github.com/lefred/MyUndelete.git
+
+
+./MyUndelete.py -b /var/log/mysql/bin-log.000001 -s 120 -e 351
