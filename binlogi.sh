260ee9cb9bb52cb3554804a5f41d407fdf1a84fb
BK <89515752+BlazejPr@users.noreply.github.com>
2025-09-29 08:19:58 +0200

    Update binlogi.sh

--- binlogi.sh
+++ binlogi.sh
@@ -12,5 +12,18 @@ https://github.com/lefred/MyUndelete.git
 
 ./MyUndelete.py -b /var/log/mysql/bin-log.000001 -s 120 -e 351
 
+SHOW VARIABLES WHERE Variable_Name='binlog_expire_logs_seconds';
+
+ SET GLOBAL binlog_expire_logs_seconds=3600;
+ SET GLOBAL binlog_expire_logs_seconds=129600;
+
+
 
 PURGE BINARY LOGS BEFORE now();
+
+SHOW BINARY LOGS;
+
+ONYMOUS' at source log binlog.004679, end_log_pos 265069270. 
+
+
+mysqlbinlog vm-10130-relay-bin.007036 --start-position=761587314 --stop-position=761587814 --base64-output=DECODE-ROWS --verbose
